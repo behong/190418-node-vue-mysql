cd15c2d0e31967fb03a5f2918b2a5984ae6da6a8
juf <sijm@naver.com>
2019-04-18 23:01:58 +0900

    json response

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const app = express();
- //testJson = require('./test/test.json');
+ testJson = require('./test/test.json');
       
 app.use(express.static('public'));
 
+app.set('views',__dirname+ '/views');
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+
 app.get('/', (req, res) => {
-    res.send("Hello NodeJS!! 한글테스트");
+    //res.send("Hello NodeJS!! 한글테스트");
+    res.json(testJson)
 });
 
  const server = app.listen(7000, function(){
