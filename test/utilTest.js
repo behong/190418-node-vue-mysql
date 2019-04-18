ecf079566461886d12a94970800e7b68c4c86f46
juf <sijm@naver.com>
2019-04-19 01:35:22 +0900

    map util

--- test/utilTest.js
+++ test/utilTest.js
@@ -1,6 +1,26 @@
 const util = require('util');
 const utils = require('../utils')
 
+
+
+
+return ;
+
+let = map = utils.makeMap('name','hong');
+util.log("map>>>>>>>>>>>>>>  ", map.get('name'));
+
+return ;
+
+let str = "NodeJS";
+let enc = utils.encrypt(str)
+util.log("enc 암호 :",utils.encrypt(enc));
+let dec = utils.decrypt(enc)
+util.log("dec 복호 :", dec);
+
+util.log("sha256 단방향 암호 :", utils.encryptSha2(str));
+
+return;
+// ogs test
 let url = "https://www.daum.net";
 
 utils.ogsinfo(url,(err,res) =>{
--- utils.js
+++ utils.js
@@ -3,10 +3,37 @@ const ogs = require('open-graph-scraper'),
       Crypto = require('crypto-js'),
       SHA256 = ('crypto-js/sha256');
 
+// 암호화 키
+const Ekey = "nodevue";
+
 module.exports = {
+    makeMap(key,value){
+        const map = new HashMap();
+        map.set(key,value);
+        console.log("key ===", map.get(key))
+        return map;
+    },
     ogsinfo(url, fn){
         return ogs({url:url},(err,ret) =>{
             fn(err,ret)
         });
+    },
+    encrypt(data,key){
+        return Crypto.AES.encrypt(data,key || Ekey).toString();
+        //return Crypto.AES.encrypt(data,key || EKey).toString();
+    },
+    decrypt(data,key){
+        //return Crypto.AES.decrypt(data,key || Ekey).toString();
+        return Crypto.AES.decrypt(data,key || Ekey).toString(Crypto.enc.Utf8);
+    },
+    //단방향 암호화
+    encryptSha2(data,key){
+        if(!data) return null;
+        key = key||Ekey;
+        try {
+            return Crypto.SHA256(data+key).toString();
+        } catch (error) {
+            console.error("Error encryptSha2 ::::" , error);
+        }
     }
-}      
+};
